3943b9da8d9b916c9cf8739743a71beb24460a77
mansoor <codehawk64@gmail.com>
2014-09-17 18:29:51 +0530

    carszx

---
+++ Assets/Looper.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Looper : MonoBehaviour {
+
+
+	public GameObject clone;
+
+
+	public bool iscloned=false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		Vector3 pos=new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z+this.transform.renderer.bounds.size.z);
+
+	
+			GameObject instance = Instantiate(clone, pos, transform.rotation) as GameObject;
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
